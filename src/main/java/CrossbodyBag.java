83c04bdc22fcc2cc033935c138191e3448b37dd3
Savanna Pan <savanna.pan@mail.utoronto.ca>
2022-09-22 20:39:09 -0400

    Exercise #3

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -56,7 +56,7 @@ public class CrossbodyBag extends Bag {
 
     @Override
     public String toString() {
-        return this.getCapacity() + "Crossbody Bag with " + this.numberOfStraps + " straps (" + this.getNumberOfContents() + " / " +
+        return this.getColor() + " Crossbody Bag with " + this.numberOfStraps + " straps (" + this.getNumberOfContents() + " / " +
                 this.getCapacity()+ ")";
     }
 
